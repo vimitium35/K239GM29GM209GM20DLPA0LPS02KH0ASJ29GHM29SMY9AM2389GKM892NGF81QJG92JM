15bb638c40a401e20c650a7b95e0b53ea3e8df6e
vimitium35 <femboydanya@gmail.com>
2026-05-16 09:14:23 +0300

    Update burmalda.lua

--- burmalda.lua
+++ burmalda.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"efe3f2","MainColor":"3c384d","AccentColor":"b38198","BackgroundColor":"2a2c38","OutlineColor":"3c384d"}') },
+		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"efe3f2","MainColor":"3c384d","AccentColor":"b38198","BackgroundColor":"3c384d","OutlineColor":"3c384d"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
